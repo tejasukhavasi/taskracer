fc732cb713c5f4bdf50fe6d547006d1db8016be3
U-NA\tsukhava <tsukhava@TSUKHAVA.na.qualcomm.com>
2012-12-08 18:21:42 -0800

    Real final version

--- src/com/qualcomm/myfirstapp/History.java
+++ src/com/qualcomm/myfirstapp/History.java
@@ -1,7 +1,7 @@
 package com.qualcomm.myfirstapp;
 
 public class History{
-	public static String[] Date = {"12/1/2012", "12/2/2012", "12/3/2012", "12/4/2012", "12/5/2012", "12/6/2012", "12/7/2012", "12/8/2012"};
+	public static String[] Date = {"Today", "12/7/2012", "12/6/2012", "12/5/2012", "12/4/2012", "12/3/2012", "12/2/2012", "12/1/2012"};
 	public static String[] Duration = {"35", "31", "34", "34", "33", "32", "30", "35"};
 	public static String[] Outcome = {"Lost", "Won", "Lost", "Lost", "Won", "Won", "Won", "Lost"};
 }
